ffd15674bef749f95fc780b838aca8ddd3d0c758
dferdez <D_ferdez@hotmail.com>
2024-08-26 19:23:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
+
 import google.generativeai as genai
-from flask import Flask, request, jsonify
+from flask import Flask,request,jsonify
 import requests
 import os
 import fitz
 
-# Configuración de las variables de entorno y la API de Gemini
-wa_token = os.environ.get("WA_TOKEN")
+wa_token=os.environ.get("WA_TOKEN")
 genai.configure(api_key=os.environ.get("GEN_API"))
-phone_id = os.environ.get("PHONE_ID")
-phone = os.environ.get("PHONE_NUMBER")
-name = "Tu Nombre"  # Cambia esto por tu nombre o apodo
-bot_name = "Nombre del Bot"  # Cambia esto por el nombre de tu bot
-model_name = "gemini-1.5-flash-latest"
+phone_id=os.environ.get("PHONE_ID")
+phone=os.environ.get("PHONE_NUMBER")
+name="Your name or nickname" #The bot will consider this person as its owner or creator
+bot_name="Give a name to your bot" #This will be the name of your bot, eg: "Hello I am Astro Bot"
+model_name="gemini-1.5-flash-latest" #Switch to "gemini-1.0-pro" or any free model, if "gemini-1.5-flash" becomes paid in future.
 
 app = Flask(__name__)
 
